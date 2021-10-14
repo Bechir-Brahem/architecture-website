735ee2b26303bf099508db7a956506b90d2c4103
SaraBriki <sarabriki@insat.u-carthage.tn>
2021-10-14 22:12:37 +0200

    contact us page

--- app.js
+++ app.js
@@ -9,4 +9,8 @@ app.use('/static', express.static('static'))
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'views', 'index.html'));
 });
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views', 'contact.html'));
+});
+
 module.exports = app;
